f71122480a19174c3af5dd96ea27469282b34532
xchanmolx <cmanigos@gmail.com>
2019-12-24 00:25:32 +0800

    Updated backend for deployment

--- server/index.js
+++ server/index.js
@@ -15,6 +15,13 @@ app.use(bodyParser.json());
 app.use('/api/cities', require('./api/cities'));
 app.use('/api/weather', require('./api/weather'));
 
+if (ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}!`);
 });
